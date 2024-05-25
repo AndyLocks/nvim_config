7017b714804a57bd3cecef5d6a538e500120b99a
Илья <112815007+AndyLocks@users.noreply.github.com>
2024-05-25 21:44:55 +0200

    Add files via upload

---
+++ lua/options.lua
@@ -0,0 +1,38 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set clipboard+=unnamedplus")
+vim.cmd("set clipboard=unnamedplus")
+vim.cmd("set encoding=utf8")
+vim.cmd("set nocompatible")
+vim.cmd("set hlsearch")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set smarttab")
+vim.cmd("set autoindent")
+vim.cmd("set smartindent")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set wildmode=longest,list")
+vim.cmd("syntax on")
+vim.cmd("set mouse=a")
+vim.cmd("set ttyfast")
+vim.cmd("set so=30")
+vim.cmd("filetype plugin indent on")
+-- vim.g.mapleader = " "
+
+--short cuts :D
+vim.keymap.set('n', '<C-b>', ':NERDTreeToggle<CR>')
+
+vim.keymap.set('n', '<C-Z>', 'u')
+vim.keymap.set('n', '<C-Y>', '<C-R>')
+vim.keymap.set('i', '<C-Z>', '<C-O>u')
+vim.keymap.set('i', '<C-Y>', '<C-O><C-R>')
+
+vim.keymap.set('n', '<C-c>', '"+y')
+vim.keymap.set('n', '<C-v>', '"+p')
+
+vim.keymap.set('n', '<C-s>', ':w')
+vim.keymap.set('n', '<C-q>', ':q')
---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,44 @@
+return {
+    {
+      "williamboman/mason.nvim",
+      lazy = false,
+      config = function()
+        require("mason").setup()
+      end,
+    },
+    {
+      "williamboman/mason-lspconfig.nvim",
+      lazy = false,
+      opts = {
+        auto_install = true,
+      },
+    },
+    {
+      "neovim/nvim-lspconfig",
+      lazy = false,
+      config = function()
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+        local lspconfig = require("lspconfig")
+        lspconfig.tsserver.setup({
+          capabilities = capabilities
+        })
+        lspconfig.html.setup({
+          capabilities = capabilities
+        })
+        lspconfig.lua_ls.setup({
+          capabilities = capabilities
+        })
+        lspconfig.dockerls.setup({
+            capabilities = capabilities
+        })
+        lspconfig.yamlls.setup({
+            capabilities = capabilities
+        })
+        lspconfig.jdtls.setup({
+            capabilities = capabilities
+        })
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      end
+    }
+  }
